c055ee33b175ed77ceba9ada204dd17f9e1ff47e
Administrator <Administrator@User-2022NBGSMH>
2022-11-02 17:36:18 +0900

    JavaPractice

---
+++ FirstProject/src/Practice02.java
@@ -0,0 +1,12 @@
+public class Practice02 {
+  public static void main(String[] args) {
+	  int time = 12;
+	  if (time < 17) {
+		  System.out.println("Good Day.");
+	  } else {
+		  System.out.println("Good Evening");
+	  }
+
+
+  }
+}
---
+++ FirstProject/src/Practice03.java
@@ -0,0 +1,16 @@
+
+public class Practice03 extends Practice01 {
+
+	public static void main(String[] args) {
+		byte var1 = 125;
+		byte var2 = 125;
+		
+		for(int i=0;i<5;i++) {
+			var1++;
+			var2++;
+			System.out.println("var1: " + var1 + "\t" + "var2:" + var2);
+		}
+
+	}
+
+}
